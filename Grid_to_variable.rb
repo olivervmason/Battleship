ea9568dd36fe0a1ba8bdf0f84fb1a966142830b1
Oliver Mason <omason@protonmail.com>
2020-03-10 15:40:42 +1100

    Added array to map user input to variables

---
+++ Grid_to_variable.rb
@@ -0,0 +1,7 @@
+
+# Each square on the battleship grid is represented by an alphanumeric grid reference (e.g. A1). These need to be mapped to the variables 1 to 100
+# created to represent their contents. 
+
+require_relative "./Grid"
+
+puts b_g[1]
---
+++ User_positions_fleet.rb
@@ -0,0 +1,22 @@
+# The user fleet consists of 5 ships:
+#     - 1 Aircraft Carrier ("C"), 5 squares long by 2 wide
+#     - 2 Destroyers ("D"), 4 squares long
+#     - 1 Submarine ("S"), 3 squares long
+#     - 2 Minesweepers, 2 squares long
+
+# Visually these will be represented on the grid by their corresponding letter, e.g for a Carrier:
+
+# AA
+# AA    AAAAA
+# AA or AAAAA
+# AA
+# AA
+
+def place_aircraft_carrier(z1, hv)
+    # To place the carrier the user specifies a particular grid reference, and then chooses whether to orientate it horizontally or vertically
+    
+    print "Select starting grid reference for your aircraft carrier: "
+    starting_gr = gets.chomp
+    
+
+end
